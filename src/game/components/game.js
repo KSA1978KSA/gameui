c614d1f9a05c3f34033027f16df4044755cde7b9
KSA1978KSA <s.kurochkin@itfbgroup.ru>
2022-09-01 10:34:33 +0300

    Game

--- src/game/components/game.js
+++ src/game/components/game.js
@@ -2,8 +2,10 @@ import React, {useState} from 'react';
 import {Puzzle} from "./puzzle.js";
 import * as all_style from "./all_style.js";
 
-
-const stateModel_Level1 = [
+//-------------------------------------
+//--- блок локальный переменных
+//-------------------------------------
+const stateModel_Level = [
     {rotate: 45, color: 1},
     {rotate: 135, color: 1},
     {rotate: 45, color: 0},
@@ -32,8 +34,10 @@ const stateModel_Level1 = [
 ];
 
 let stateModel=[];
-let timerFunction;
-
+let timerGame;
+let timerGenerateGame;
+let startGame = false;//--- флаг старта игры
+//-------------------------------------
 
 //--- Генератор случайных чисел в диапазон 0-max
 function getRandomInt(max, exclude) {
@@ -196,101 +200,80 @@ function getExcludeGradientColors (hElement, vElement) {
 }
 
 
+
+
+//--------------------------------
+//--- Функция генерации уровня
+//-------------------------------- 
+function GenerateLevelGame(stateModel_hook) {  
+
+    //--- первый ряд
+    stateModel_Level[0].color = getRandomInt(4,[]);
+    stateModel_Level[0].rotate = 45;//--- стартовый поворот
+
+    let nextElement = getExcludeGradientColors(stateModel_Level[0], "");
+    stateModel_Level[1].rotate = nextElement.rotateNextColor;
+    stateModel_Level[1].color = getRandomInt(4, nextElement.excludeColors);
+    
+    nextElement = getExcludeGradientColors(stateModel_Level[1], "");
+    stateModel_Level[2].rotate = nextElement.rotateNextColor;
+    stateModel_Level[2].color = getRandomInt(4, nextElement.excludeColors);
+
+    nextElement = getExcludeGradientColors(stateModel_Level[2], "");
+    stateModel_Level[3].rotate = nextElement.rotateNextColor;
+    stateModel_Level[3].color = getRandomInt(4, nextElement.excludeColors);
+
+    nextElement = getExcludeGradientColors(stateModel_Level[3], "");
+    stateModel_Level[4].rotate = nextElement.rotateNextColor;
+    stateModel_Level[4].color = getRandomInt(4, nextElement.excludeColors);
+
+    //--- последующие ряды
+    for (let step=1;step<5;step++) {
+        nextElement = getExcludeGradientColors("", stateModel_Level[(step - 1) * 5 + 0]);
+        stateModel_Level[step * 5 + 0].rotate = nextElement.rotateNextColor;
+        stateModel_Level[step * 5 + 0].color = getRandomInt(4, nextElement.excludeColors);
+
+        nextElement = getExcludeGradientColors(stateModel_Level[step * 5 + 0], stateModel_Level[(step - 1) * 5 + 1]);
+        stateModel_Level[step * 5 + 1].rotate = nextElement.rotateNextColor;
+        stateModel_Level[step * 5 + 1].color = getRandomInt(4, nextElement.excludeColors);
+
+        nextElement = getExcludeGradientColors(stateModel_Level[step * 5 + 1], stateModel_Level[(step - 1) * 5 + 2]);
+        stateModel_Level[step * 5 + 2].rotate = nextElement.rotateNextColor;
+        stateModel_Level[step * 5 + 2].color = getRandomInt(4, nextElement.excludeColors);
+
+        nextElement = getExcludeGradientColors(stateModel_Level[step * 5 + 2], stateModel_Level[(step - 1) * 5 + 3]);
+        stateModel_Level[step * 5 + 3].rotate = nextElement.rotateNextColor;
+        stateModel_Level[step * 5 + 3].color = getRandomInt(4, nextElement.excludeColors);
+
+        nextElement = getExcludeGradientColors(stateModel_Level[step * 5 + 3], stateModel_Level[(step - 1) * 5 + 4]);
+        stateModel_Level[step * 5 + 4].rotate = nextElement.rotateNextColor;
+        stateModel_Level[step * 5 + 4].color = getRandomInt(4, nextElement.excludeColors);
+    }
+
+    //--- переносим в статусную модель в хуке
+    for (let index = 0;index<stateModel_Level.length;index++) {
+        stateModel_hook[index].rotate = stateModel_Level[index].rotate;
+        stateModel_hook[index].color = stateModel_Level[index].color;
+    }    
+    
+}
+
+
 /*    
     Компонента с игровой логикой
 */
 function Game() {
-
-    //--------------------------------
-    //--- блок генерации уровня
-    //--------------------------------    
-
-    //--- первый ряд
-    stateModel_Level1[0].color = getRandomInt(4,[]);
-    stateModel_Level1[0].rotate = 45;//--- стартовый поворот
-
-    let nextElement = getExcludeGradientColors(stateModel_Level1[0], "");
-    stateModel_Level1[1].rotate = nextElement.rotateNextColor;
-    stateModel_Level1[1].color = getRandomInt(4, nextElement.excludeColors);
     
-    nextElement = getExcludeGradientColors(stateModel_Level1[1], "");
-    stateModel_Level1[2].rotate = nextElement.rotateNextColor;
-    stateModel_Level1[2].color = getRandomInt(4, nextElement.excludeColors);
 
-    nextElement = getExcludeGradientColors(stateModel_Level1[2], "");
-    stateModel_Level1[3].rotate = nextElement.rotateNextColor;
-    stateModel_Level1[3].color = getRandomInt(4, nextElement.excludeColors);
-
-    nextElement = getExcludeGradientColors(stateModel_Level1[3], "");
-    stateModel_Level1[4].rotate = nextElement.rotateNextColor;
-    stateModel_Level1[4].color = getRandomInt(4, nextElement.excludeColors);
-
-    let hIndex = 0;
-    let vIndex = 0;
-
-    for (let step=1;step<5;step++) {
-        nextElement = getExcludeGradientColors("", stateModel_Level1[(step - 1) * 5 + 0]);
-        stateModel_Level1[step * 5 + 0].rotate = nextElement.rotateNextColor;
-        stateModel_Level1[step * 5 + 0].color = getRandomInt(4, nextElement.excludeColors);
-
-        nextElement = getExcludeGradientColors(stateModel_Level1[step * 5 + 0], stateModel_Level1[(step - 1) * 5 + 1]);
-        stateModel_Level1[step * 5 + 1].rotate = nextElement.rotateNextColor;
-        stateModel_Level1[step * 5 + 1].color = getRandomInt(4, nextElement.excludeColors);
-
-        nextElement = getExcludeGradientColors(stateModel_Level1[step * 5 + 1], stateModel_Level1[(step - 1) * 5 + 2]);
-        stateModel_Level1[step * 5 + 2].rotate = nextElement.rotateNextColor;
-        stateModel_Level1[step * 5 + 2].color = getRandomInt(4, nextElement.excludeColors);
-
-        nextElement = getExcludeGradientColors(stateModel_Level1[step * 5 + 2], stateModel_Level1[(step - 1) * 5 + 3]);
-        stateModel_Level1[step * 5 + 3].rotate = nextElement.rotateNextColor;
-        stateModel_Level1[step * 5 + 3].color = getRandomInt(4, nextElement.excludeColors);
-
-        nextElement = getExcludeGradientColors(stateModel_Level1[step * 5 + 3], stateModel_Level1[(step - 1) * 5 + 4]);
-        stateModel_Level1[step * 5 + 4].rotate = nextElement.rotateNextColor;
-        stateModel_Level1[step * 5 + 4].color = getRandomInt(4, nextElement.excludeColors);
-    }
-
-    /*
-    nextElement = getExcludeGradientColors("", stateModel_Level1[5]);
-    stateModel_Level1[10].rotate = nextElement.rotateNextColor;
-    stateModel_Level1[10].color = getRandomInt(4, nextElement.excludeColors);
-*/
-
-    /*
-    //--- второй ряд
-    nextElement = getExcludeGradientColors("", stateModel_Level1[0]);
-    stateModel_Level1[5].rotate = nextElement.rotateNextColor;
-    stateModel_Level1[5].color = getRandomInt(4, nextElement.excludeColors);
-
-    nextElement = getExcludeGradientColors(stateModel_Level1[5], stateModel_Level1[1]);
-    stateModel_Level1[6].rotate = nextElement.rotateNextColor;
-    stateModel_Level1[6].color = getRandomInt(4, nextElement.excludeColors);
-
-    nextElement = getExcludeGradientColors(stateModel_Level1[6], stateModel_Level1[2]);
-    stateModel_Level1[7].rotate = nextElement.rotateNextColor;
-    stateModel_Level1[7].color = getRandomInt(4, nextElement.excludeColors);
-
-    nextElement = getExcludeGradientColors(stateModel_Level1[7], stateModel_Level1[3]);
-    stateModel_Level1[8].rotate = nextElement.rotateNextColor;
-    stateModel_Level1[8].color = getRandomInt(4, nextElement.excludeColors);
-
-    nextElement = getExcludeGradientColors(stateModel_Level1[8], stateModel_Level1[4]);
-    stateModel_Level1[9].rotate = nextElement.rotateNextColor;
-    stateModel_Level1[9].color = getRandomInt(4, nextElement.excludeColors);
-*/
-    //--------------------------------
-
-
-
-    //---- инициализируем первый уровень 
-    stateModel = stateModel_Level1.map ((el) => (
+    //---- инициализируем уровень 
+    stateModel = stateModel_Level.map ((el) => (
         {
             rotate : el.rotate,
             color: el.color
         }
-    ));//---- инициализируем первый уровень        
+    ));//---- инициализируем уровень   
 
-
+    
 
     const [
         stateModel_hook, //--- объявляем через деструктуризацию переменную со статусом
@@ -299,8 +282,26 @@ function Game() {
 
     const refresh_all_model = function () {
         const stateModel_hook_new = [...stateModel_hook];//--- передавать нужно обязательно новый экземпляр!!!
-        set_hook (stateModel_hook_new);  
+        set_hook (stateModel_hook_new);         
     }
+
+    
+    //--- так как вызов Game() происходит после каждой перерисовки (render)
+    //--- то нужно деактивировать таймер и заново активировать
+    clearInterval(timerGenerateGame);//--- останавливаем таймер игры
+
+
+    if (startGame===false) {  
+        clearInterval(timerGame);//--- останавливаем таймер игры
+
+        timerGenerateGame = setInterval(() => {
+            //--- тут мы красиво генерим уровни  
+            GenerateLevelGame(stateModel_hook);                                                 
+            refresh_all_model();      
+        }
+        , 500);
+    }
+    
 
 
     //---- функция обратного вызова из дочерних компонент-puzzle
@@ -333,26 +334,37 @@ function Game() {
                 <all_style.rootPano__leftPanel_topPanel>            
                     <button className='buttonClear'
                         onClick={() => {
+                            startGame = false;
+
                             for (let i=0;i<stateModel_hook.length;i++) {                            
-                                stateModel_hook[i].rotate = stateModel_Level1[i].rotate;                           
+                                stateModel_hook[i].rotate = stateModel_Level[i].rotate;                           
                             };
-                            refresh_all_model();   
-                            clearInterval(timerFunction);
+                            refresh_all_model();                               
                         }   
                     }
                     >Сбросить</button>
 
                     <button className='buttonStart'
                         onClick={() => {
+                            startGame = true;
+
+                            clearInterval(timerGenerateGame);//--- останавливаем таймер генерации уровней игры
+
                             for (let i=0;i<stateModel_hook.length;i++) {
                                 stateModel_hook[i].rotate = 0;                           
                             };
                             refresh_all_model(); 
-                            timerFunction = setInterval(() => {
+
+                            
+
+                            timerGame = setInterval(() => {
                                 //--- тут делаем проверку на завершение уровня
-                                if (JSON.stringify(stateModel_hook)===JSON.stringify(stateModel_Level1)) {
+                                if (JSON.stringify(stateModel_hook)===JSON.stringify(stateModel_Level)) {
                                     alert("Ура! Вы успешно собрали мозаику!!!");
-                                    clearInterval(timerFunction);
+                                    clearInterval(timerGame);
+                                    startGame = false;
+                                    refresh_all_model(); 
+
                                 }
                             }
                             , 1000);                        
